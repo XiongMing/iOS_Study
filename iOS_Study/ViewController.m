145bbf622148c3c2019127363a488eaa502f8aef
XiongMing <chexming@gmail.com>
2015-04-21 18:38:05 +0800

    UILabel test

--- iOS_Study/ViewController.m
+++ iOS_Study/ViewController.m
@@ -17,6 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    NSString *s = @"凌波微步";
+    UIFont *font = [UIFont fontWithName:@"Arial" size:50.0f];
+    CGSize size = CGSizeMake(320, 2000);
+    UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectZero];
+    [label2 setNumberOfLines:0];
+    CGSize labelsize = [s sizeWithFont:font
+                     constrainedToSize:size
+                         lineBreakMode:UILineBreakModeWordWrap];
+    label2.frame = CGRectMake(0, 0, labelsize.width, labelsize.height);
+    label2.textColor = [UIColor blackColor];
+    label2.font = font;
+    label2.text = s;
+    [self.view addSubview:label2];
 }
 
 - (void)didReceiveMemoryWarning {
