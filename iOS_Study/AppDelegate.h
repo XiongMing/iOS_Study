827ff6b10a0ec81656c3ae1a21778748696b3804
XiongMing <chexming@gmail.com>
2015-04-21 18:22:24 +0800

    Initial Commit

---
+++ iOS_Study/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_Study
+//
+//  Created by xiongming on 2015/4/21.
+//  Copyright (c) 2015年 xiongming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
